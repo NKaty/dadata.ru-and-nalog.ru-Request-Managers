87fcd5ecaf747ee98793b397d14b6cbcb9e4d68b
NKaty <nd.katy@gmail.com>
2020-10-09 22:47:08 +0300

    Add comments to worker

--- src/parser/worker.js
+++ src/parser/worker.js
@@ -1,3 +1,6 @@
+// Worker to parse egrul pdf documents
+// Worker is designed to work with WorkerPool class
+
 const { parentPort } = require('worker_threads');
 
 const Parser = require('./Parser');
@@ -5,10 +8,15 @@ const Parser = require('./Parser');
 const parser = new Parser();
 
 parentPort.on('message', async (path) => {
+  // As this listener is an async function, the onError listener in the main thread
+  // doesn't catch errors occurred here (inside this listener).
+  // So we must catch errors by ourselves
   try {
     const result = await parser.parse(path);
+    // Send the data object to the main thread
     parentPort.postMessage({ status: 'success', data: result });
   } catch (error) {
+    // Catch an error, send it to the main thread
     parentPort.postMessage({ status: 'error', error });
   }
 });
